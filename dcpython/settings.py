df4fc474f48eb1fdb730a9ed3ae126507f412522
Josh Finnie <joshua.finnie@gmail.com>
2014-08-02 13:56:52 -0400

    setting login redirect to homepage

--- dcpython/settings.py
+++ dcpython/settings.py
@@ -204,6 +204,8 @@ AUTHENTICATION_BACKENDS = (
     "allauth.account.auth_backends.AuthenticationBackend",
 )
 
+LOGIN_REDIRECT_URL = '/'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
