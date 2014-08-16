4e7e6e01a1af3159366b4ba1c146813940cc17ee
Davis Silverman <sinistersnare@gmail.com>
2014-08-16 13:37:04 -0400

    moved support urls to support/urls.py

--- dcpython/urls.py
+++ dcpython/urls.py
@@ -8,13 +8,9 @@ urls = ()
 
 urlpatterns = patterns('',
     url(r'^$', 'dcpython.app.views.home', name='home'),
-    url(r'^events/', include("dcpython.events.urls")),
-    url(r'^blog/', include("dcpython.blog.urls")),
-    # FIXME: refactor all donation views into support/urls.py, consider renaming app
-    url(r'^donate/$', 'dcpython.support.views.support', name='support'),
-    url(r'^andrew-w-singer.html', 'dcpython.support.views.andrew_w_singer', name='andrew_w_singer'),
-    url(r'^donor/(?P<secret>[\w\-_]+=?=?=?)/$', 'dcpython.support.views.donor_update', name='donor_update'),
-    url(r'^make_donation$', 'dcpython.support.views.make_donation', name='make_donation'),
+    url(r'^events/', include('dcpython.events.urls')),
+    url(r'^blog/', include('dcpython.blog.urls')),
+    url(r'^donate/', include('dcpython.donate.urls')),
     url(r'^about/$', 'dcpython.app.views.about', name='about'),
     url(r'^deals/$', 'dcpython.app.views.deals', name='deals'),
     url(r'^resources/$', 'dcpython.app.views.resources', name='resources'),
