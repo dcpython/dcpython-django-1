e3637928ced8e132bd3bfdaa9a75d6b45813950e
Alex Clark <aclark@aclark.net>
2015-10-23 19:19:22 -0400

    Update

--- dcpython/settings.py
+++ dcpython/settings.py
@@ -105,3 +105,16 @@ STATIC_URL = '/static/'
 # DC Python
 
 MEETUP_API_KEY = os.getenv('MEETUP_API_KEY', '')
+
+# https://devcenter.heroku.com/articles/django-assets
+#
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
