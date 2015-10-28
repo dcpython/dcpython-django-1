6eb8fc8f5032389a84bfa8b1386e363afc239074
Alex Clark <aclark@aclark.net>
2015-10-28 19:22:02 -0400

    Upgrade to Django 1.8 migrations

--- dcpython/app/migrations/0001_initial.py
+++ dcpython/app/migrations/0001_initial.py
@@ -1,117 +1,115 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import localflavor.us.models
+import django.core.files.storage
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'User'
-        db.create_table(u'app_user', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('password', self.gf('django.db.models.fields.CharField')(max_length=128)),
-            ('last_login', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
-            ('is_superuser', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('username', self.gf('django.db.models.fields.CharField')(unique=True, max_length=30)),
-            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
-            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
-            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
-            ('is_staff', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('date_joined', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
-            ('meetup_username', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
-            ('meetup_id', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
-        ))
-        db.send_create_signal(u'app', ['User'])
+    dependencies = [
+    ]
 
-        # Adding M2M table for field groups on 'User'
-        m2m_table_name = db.shorten_name(u'app_user_groups')
-        db.create_table(m2m_table_name, (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('user', models.ForeignKey(orm[u'app.user'], null=False)),
-            ('group', models.ForeignKey(orm[u'auth.group'], null=False))
-        ))
-        db.create_unique(m2m_table_name, ['user_id', 'group_id'])
-
-        # Adding M2M table for field user_permissions on 'User'
-        m2m_table_name = db.shorten_name(u'app_user_user_permissions')
-        db.create_table(m2m_table_name, (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('user', models.ForeignKey(orm[u'app.user'], null=False)),
-            ('permission', models.ForeignKey(orm[u'auth.permission'], null=False))
-        ))
-        db.create_unique(m2m_table_name, ['user_id', 'permission_id'])
-
-        # Adding model 'ServiceSync'
-        db.create_table(u'app_servicesync', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('service', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('last_synced', self.gf('django.db.models.fields.CharField')(max_length=50)),
-        ))
-        db.send_create_signal(u'app', ['ServiceSync'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'User'
-        db.delete_table(u'app_user')
-
-        # Removing M2M table for field groups on 'User'
-        db.delete_table(db.shorten_name(u'app_user_groups'))
-
-        # Removing M2M table for field user_permissions on 'User'
-        db.delete_table(db.shorten_name(u'app_user_user_permissions'))
-
-        # Deleting model 'ServiceSync'
-        db.delete_table(u'app_servicesync')
-
-
-    models = {
-        u'app.servicesync': {
-            'Meta': {'object_name': 'ServiceSync'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'last_synced': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'service': ('django.db.models.fields.CharField', [], {'max_length': '200'})
-        },
-        u'app.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'meetup_id': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'meetup_username': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        u'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        u'auth.permission': {
-            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        }
-    }
-
-    complete_apps = ['app']
+    operations = [
+        migrations.CreateModel(
+            name='Donation',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('datetime', models.DateTimeField()),
+                ('type', models.CharField(max_length=1, choices=[(b'C', b'Credit Card'), (b'G', b'Pledge')])),
+                ('completed', models.BooleanField(default=False)),
+                ('donation', models.DecimalField(max_digits=10, decimal_places=2)),
+                ('transaction_id', models.CharField(max_length=50)),
+                ('valid_until', models.DateField(null=True, blank=True)),
+                ('level', models.CharField(blank=True, max_length=1, null=True, choices=[(b'A', b'Andrew W. Singer Memorial Level ($2500)'), (b'P', b'Platinum ($1000)'), (b'G', b'Gold ($500)'), (b'S', b'Silver ($250)'), (b'B', b'Bronze ($100)'), (b'I', b'Individual ($50)'), (b'O', b'Other ($0)')])),
+                ('reviewed', models.BooleanField(default=False)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Donor',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('email', models.EmailField(max_length=254)),
+                ('phone', localflavor.us.models.PhoneNumberField(max_length=20, null=True, blank=True)),
+                ('name', models.CharField(help_text=b"If institutional donation, point of contact's name", max_length=100)),
+                ('public_name', models.CharField(max_length=100, null=True, verbose_name=b'Display Name', blank=True)),
+                ('public_url', models.URLField(null=True, verbose_name=b'Display Url', blank=True)),
+                ('public_slogan', models.CharField(max_length=200, null=True, verbose_name=b'Display Slogan', blank=True)),
+                ('public_logo', models.ImageField(storage=django.core.files.storage.FileSystemStorage(), upload_to=b'donor_logos', null=True, verbose_name=b'Display Logo', blank=True)),
+                ('level', models.CharField(blank=True, max_length=1, null=True, help_text=b"Override levels specified by donations if not past 'valid until'", choices=[(b'A', b'Andrew W. Singer Memorial Level ($2500)'), (b'P', b'Platinum ($1000)'), (b'G', b'Gold ($500)'), (b'S', b'Silver ($250)'), (b'B', b'Bronze ($100)'), (b'I', b'Individual ($50)'), (b'O', b'Other ($0)')])),
+                ('secret', models.CharField(max_length=90)),
+                ('reviewed', models.BooleanField(default=False)),
+                ('valid_until', models.DateField(help_text=b'Specify a date until which the level specified for the donor is valid. After, donation levels will control.', null=True, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Event',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('record_created', models.DateTimeField(auto_now_add=True)),
+                ('record_modified', models.DateTimeField(auto_now=True)),
+                ('meetup_id', models.CharField(unique=True, max_length=32)),
+                ('name', models.CharField(max_length=200)),
+                ('slug', models.SlugField(max_length=200)),
+                ('start_time', models.DateTimeField(null=True)),
+                ('end_time', models.DateTimeField(null=True, blank=True)),
+                ('meetup_url', models.URLField(blank=True)),
+                ('description', models.TextField(null=True, blank=True)),
+            ],
+            options={
+                'ordering': ('start_time', 'end_time'),
+                'get_latest_by': 'start_time',
+            },
+        ),
+        migrations.CreateModel(
+            name='Playlist',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('remote_id', models.CharField(max_length=100, null=True, blank=True)),
+                ('updated', models.CharField(max_length=30)),
+                ('event', models.ForeignKey(related_name='playlists', to='app.Event')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='ServiceSync',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('service', models.CharField(max_length=200)),
+                ('last_synced', models.CharField(max_length=50)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Venue',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('meetup_id', models.CharField(unique=True, max_length=32)),
+                ('lon', models.DecimalField(null=True, max_digits=9, decimal_places=6)),
+                ('lat', models.DecimalField(null=True, max_digits=9, decimal_places=6)),
+                ('name', models.CharField(max_length=200)),
+                ('state', models.CharField(max_length=2)),
+                ('address_1', models.CharField(max_length=200)),
+                ('address_2', models.CharField(max_length=200, blank=True)),
+                ('address_3', models.CharField(max_length=200, blank=True)),
+                ('phone', models.CharField(default=b'', max_length=15, blank=True)),
+                ('city', models.CharField(max_length=100)),
+                ('country', models.CharField(max_length=2)),
+                ('zip_code', models.CharField(max_length=5, blank=True)),
+                ('repinned', models.BooleanField()),
+            ],
+        ),
+        migrations.AddField(
+            model_name='event',
+            name='venue',
+            field=models.ForeignKey(blank=True, to='app.Venue', null=True),
+        ),
+        migrations.AddField(
+            model_name='donation',
+            name='donor',
+            field=models.ForeignKey(related_name='donations', to='app.Donor'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='event',
+            unique_together=set([('start_time', 'slug')]),
+        ),
+    ]
